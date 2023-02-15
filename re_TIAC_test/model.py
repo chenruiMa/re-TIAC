7b9704fb33f695a4803296c4e686fa21ce12a952
cherry <cherry@cherrydeMacBook-Air.local>
2023-02-15 16:02:02 +0800

    first commit

---
+++ re_TIAC_test/model.py
@@ -0,0 +1,300 @@
+import numpy as np
+import torch
+import sys
+import torch.nn as nn
+from utils import *
+import math
+import torch.nn.functional as F
+
+FLOAT_MIN = -sys.float_info.max
+
+
+class HTP(torch.nn.Module):
+    def __init__(self, user_num, item_num,cate_num, yearnum, monthnum, daynum, args,time_int,item_time_matirx,norm_adj,uc_adj):
+        super(HTP, self).__init__()
+
+        self.user_num = user_num
+        self.item_num = item_num
+        self.cate_num = cate_num
+        self.year_num = yearnum
+        self.day_num = daynum
+        self.month_num = monthnum
+        self.time_int = time_int
+
+
+        self.args = args
+
+        self.dev = args.device
+        self.beta = args.beta
+        self.item_time_matrix = item_time_matirx.to(self.dev)
+        #graph
+        self.weight_size = [args.hidden_units, args.hidden_units, args.hidden_units, args.hidden_units]
+        dropout_list = [0.1, 0.1, 0.1, 0.1]
+        self.norm_adj = norm_adj.to(self.dev)
+        self.uc_adj = uc_adj.to(self.dev)
+
+        self.n_layers = self.args.gcn_layer
+        self.dropout_list = nn.ModuleList()
+        self.GC_Linear_list = nn.ModuleList()
+        self.Bi_Linear_list = nn.ModuleList()
+        self.weight_size = [args.hidden_units] + self.weight_size
+        for i in range(self.n_layers):
+            self.GC_Linear_list.append(nn.Linear(self.weight_size[i], self.weight_size[i+1]))
+            self.Bi_Linear_list.append(nn.Linear(self.weight_size[i], self.weight_size[i+1]))
+            self.dropout_list.append(nn.Dropout(dropout_list[i]))
+
+
+
+        # TODO: loss += args.l2_emb for regularizing embedding vectors during training
+        # https://stackoverflow.com/questions/42704283/adding-l1-l2-regularization-in-pytorch
+        self.item_emb = torch.nn.Embedding(self.item_num, args.hidden_units, padding_idx=0)
+        self.user_emb = torch.nn.Embedding(self.user_num, args.hidden_units, padding_idx=0)
+        self.category_emb = torch.nn.Embedding(self.cate_num, args.hidden_units, padding_idx=0)
+        self.year_emb = torch.nn.Embedding(self.year_num, args.hidden_units, padding_idx=0)
+        self.month_emb = torch.nn.Embedding(self.month_num, args.hidden_units, padding_idx=0)
+        self.day_emb = torch.nn.Embedding(self.day_num, args.hidden_units, padding_idx=0)
+        self.mu_all = torch.nn.Embedding(self.user_num, args.maxlen)
+        self.sigma_all = torch.nn.Embedding(self.user_num, args.maxlen)
+
+        self.item_emb_dropout = torch.nn.Dropout(p=args.dropout_rate)
+        self.year_emb_dropout = torch.nn.Dropout(p=args.dropout_rate)
+        self.month_emb_dropout = torch.nn.Dropout(p=args.dropout_rate)
+        self.day_emb_dropout = torch.nn.Dropout(p=args.dropout_rate)
+
+        # position encoding
+        self.abs_pos_emb = torch.nn.Embedding(args.maxlen, args.hidden_units)
+        self.abs_pos_emb_dropout = torch.nn.Dropout(p=args.dropout_rate)
+
+        self.last_layernorm = torch.nn.LayerNorm(args.hidden_units, eps=1e-8)
+
+        self.GRU = torch.nn.GRU(input_size=args.hidden_units, hidden_size=args.hidden_units,
+                                num_layers=1, batch_first=True)
+        self.softmax = torch.nn.Softmax(dim=-1)
+
+        self._init_weight_()
+
+    def _init_weight_(self):
+        nn.init.xavier_uniform_(self.user_emb.weight)
+        nn.init.xavier_uniform_(self.item_emb.weight)
+
+    def seq2feats(self, user_ids, log_seqs, year, month, day):
+        train = True
+        if log_seqs.shape[0] == 1:
+            train = False
+        # item embedding
+        user_emb ,items_emb = self.UI(self.norm_adj)
+        # user_emb, categry_emb = self.UC(self.uc_adj)
+        # con_loss2 = self.SSL_ci(user_emb , user_emb_c)
+        
+        # items_emb = self.item_emb.weight
+        seqs = items_emb[log_seqs].to(self.dev)
+        # seqs = items_emb(torch.LongTensor(log_seqs).to(self.dev))
+        
+        seqs = seqs * self.item_emb.embedding_dim ** 0.5
+        seqs = self.item_emb_dropout(seqs)
+
+        # position encoding
+        positions = np.tile(np.array(range(log_seqs.shape[1])), [log_seqs.shape[0], 1])
+        positions = torch.LongTensor(positions).to(self.dev)
+        abs_pos_embs = self.abs_pos_emb(positions)
+        abs_pos_embs = self.abs_pos_emb_dropout(abs_pos_embs)
+
+        # time embedding
+        year_embs = self.year_emb(torch.LongTensor(year).to(self.dev))
+        month_embs = self.month_emb(torch.LongTensor(month).to(self.dev))
+        day_embs = self.day_emb(torch.LongTensor(day).to(self.dev))
+
+        times_emb = torch.cat((self.year_emb.weight, self.month_emb.weight, self.day_emb.weight), dim=0)
+        item_time_embs = torch.sparse.mm(self.item_time_matrix, times_emb)
+        if train:
+            item_time_emb = item_time_embs[log_seqs]
+        else:
+            item_time_emb = item_time_embs[log_seqs].unsqueeze(0)
+            
+        seqs = seqs + abs_pos_embs+item_time_emb
+#         seqs = seqs + abs_pos_embs
+
+        year_embs = self.year_emb_dropout(year_embs)
+        month_embs = self.month_emb_dropout(month_embs)
+        day_embs = self.day_emb_dropout(day_embs)
+        
+        time_embs = month_embs + day_embs
+
+#         # history time
+        history_time_embs = time_embs[:, :self.args.maxlen]       # B * maxlen * d
+#         # target time
+        perdiction_time_embs = time_embs[:, 1:self.args.maxlen+1]   # B * maxlen * d
+
+        
+        timeline_mask = torch.BoolTensor(log_seqs == 0).to(self.dev)  # B * len
+        seqs *= ~timeline_mask.unsqueeze(-1)  # broadcast in last dim
+
+        tl = seqs.shape[1]  # time dim len for enforce causality  # maxlen
+        attention_mask = ~torch.tril(torch.ones((tl, tl), dtype=torch.bool, device=self.dev))
+
+        # compute time interval matrix
+        src_time_embs = history_time_embs.unsqueeze(1)  # B * 1 * N * dim
+        dst_time_embs = history_time_embs.unsqueeze(2)  # B * N * 1 * dim
+        # time_matrices = src_time_embs - dst_time_embs
+
+        Fu, _ = self.GRU(seqs)
+        Gu = user_emb[user_ids]
+        Gu = Gu.unsqueeze(1)
+        
+#         con_loss = 0
+        int_seq = Fu - seqs
+#         Iu,_ = self.GRU(int_seq)
+
+        self.delta_t = torch.Tensor(self.time_int[user_ids]).to(self.dev)
+        mu_all = self.mu_all.weight[user_ids].to(self.dev)
+        sigma_all = self.sigma_all.weight[user_ids].to(self.dev)
+        
+        E_recom = self.perdiction_time_process(perdiction_time_embs, history_time_embs, seqs, Fu, attention_mask,mu_all,sigma_all)
+        E_recom = self.last_layernorm(E_recom)
+        con_loss = self.SSL(Fu , Gu)
+        # Fusion
+        log_feats = E_recom+self.last_layernorm(Fu)
+#         log_feats = E_recom
+        return log_feats, self.beta*con_loss, items_emb
+
+    def forward(self, user_ids, log_seqs, year, month, day, pos_seqs, neg_seqs):  # for training
+        log_feats , con_loss, items_emb = self.seq2feats(user_ids, log_seqs, year, month, day)
+        
+        pos_seqs = torch.LongTensor(pos_seqs).to(self.dev)
+
+        pos_embs = self.item_emb(pos_seqs)  # B *  N * d
+#         pos_embs = items_emb[pos_seqs]
+        neg_seqs = torch.LongTensor(neg_seqs).to(self.dev)
+        # print(neg_seqs.shape, neg_seqs.max(), self.item_num, neg_seqs.min(), self.item_emb.weight.shape)
+        neg_embs = self.item_emb(neg_seqs)
+#         neg_embs = items_emb[neg_seqs]
+        pos_logits = (log_feats * pos_embs).sum(dim=-1)#128*25*50   *   128*25*50  =128*25
+        neg_logits = (log_feats * neg_embs).sum(dim=-1)
+
+        return pos_logits, neg_logits, con_loss  # pos_pred, neg_pred
+
+    def predict(self, user_ids, log_seqs, item_indices, year, month, day):  # for inference
+
+        log_feats,con_loss,items_emb = self.seq2feats(user_ids, log_seqs, year, month, day, )
+
+        final_feat = log_feats[:, -1, :]  # only use last QKV classifier, a waste
+
+        item_embs = self.item_emb(torch.LongTensor(item_indices).to(self.dev))  # (U, I, C)
+#         item_embs = items_emb(torch.LongTensor(item_indices).to(self.dev))
+        
+        logits = item_embs.matmul(final_feat.unsqueeze(-1)).squeeze(-1)
+
+        # preds = self.pos_sigmoid(logits) # rank same item list for different users
+
+        return logits  # preds # (U, I)
+    def perdiction_time_process(self, per_time_embs, history_time_embs, item_embs, Fu, attention_mask, mu, sigma):
+        # 计算时间的影响因子
+        src_time_embs = per_time_embs.unsqueeze(1)  # B * 1 * N * dim
+        dst_time_embs = history_time_embs.unsqueeze(2)  # B * N * 1 * dim
+        time_embs = (src_time_embs - dst_time_embs).sum(-1)  # B * N * N * d
+
+        paddings = torch.ones(time_embs.shape) * (-2 ** 32 + 1)  # -1e23 # float('-inf')
+        paddings = paddings.to(self.dev)
+        attn_weights = torch.where(attention_mask, paddings, time_embs)  # enforcing causality
+        intent_attention = torch.matmul(Fu, item_embs.permute(0, 2, 1))  # B* N * N
+        # print(intent_attention.shape, paddings.shape, attention_mask.shape)
+        attn_weights = torch.where(attention_mask, paddings, intent_attention)   # enforcing causality
+        intent_attention = self.softmax(attn_weights)
+
+        #核函数变化
+        att_weight = intent_attention[:, -1, :]
+        pi = 3.14
+        # mus = mu.detach().numpy()
+        # sigmas = sigma.detach().numpy()
+        change = -pi * torch.exp(-(self.delta_t - mu) ** 2 / (2 * sigma ** 2)/1000) / (math.sqrt(2 * pi) * sigma)
+        change = change.to(self.dev)
+        intent_attentions = att_weight + change
+        intent_attentions = intent_attentions.unsqueeze(-1).repeat(1, 1, self.args.maxlen)
+        final_intent = self.softmax(intent_attentions)
+        W_i = 1
+        final_intent = W_i*final_intent
+#         final_intent = intent_attention
+# #         计算聚合得分
+        embs = torch.matmul(final_intent, item_embs)
+#         embs = torch.matmul(intent_attention, item_embs)
+        
+        return embs
+
+    def SSL(self, Fu, Gu):#Fu , Gu
+        def row_shuffle(embedding):
+            corrupted_embedding = embedding[torch.randperm(embedding.size()[0])]
+            return corrupted_embedding
+        def row_column_shuffle(embedding):
+            corrupted_embedding = embedding[torch.randperm(embedding.size()[0])]
+            corrupted_embedding = corrupted_embedding[:,torch.randperm(corrupted_embedding.size()[1])]
+            corrupted_embedding = corrupted_embedding[:,:,torch.randperm(corrupted_embedding.size()[2])]
+            return corrupted_embedding
+        def score(x1, x2):
+            return torch.mean(torch.mul(x1, x2), 1)
+#             return torch.cosine_similarity(x1, x2, dim=0)
+        Fu = Fu[:,-1,:]
+        pos = score(Fu, Gu).to(self.dev)
+        neg1 = score(Fu, row_column_shuffle(Gu)).to(self.dev)
+        one = torch.FloatTensor(pos.shape).fill_(1).to(self.dev)
+        # one = zeros = torch.ones(neg1.shape[0])
+        con_loss = torch.mean(-torch.log(1e-8 + torch.sigmoid(pos))-torch.log(1e-8 + (one - torch.sigmoid(neg1))))#将-换成+
+        return con_loss
+
+    def SSL_ci(self, u_i, u_c):#Fu , Gu
+        def row_shuffle(embedding):
+            corrupted_embedding = embedding[torch.randperm(embedding.size()[0])]
+            return corrupted_embedding
+        def score(x1, x2):
+            return torch.mean(torch.mul(x1, x2), 1)
+#             return torch.cosine_similarity(x1, x2, dim=0)
+        pos = score(u_i, u_c).to(self.dev)
+        neg1 = score(u_i, row_shuffle(u_c)).to(self.dev)
+        one = torch.FloatTensor(pos.shape).fill_(1).to(self.dev)
+        # one = zeros = torch.ones(neg1.shape[0])
+        con_loss = torch.mean(-torch.log(1e-8 + torch.sigmoid(pos))-torch.log(1e-8 + (one - torch.sigmoid(neg1))))#将-换成+
+        return con_loss
+
+    #NGCF user and item
+    def UI(self,adj):
+        ego_embeddings = torch.cat((self.user_emb.weight, self.item_emb.weight), dim=0).to(self.dev)
+        all_embeddings = [ego_embeddings]
+        for i in range(self.n_layers):
+            side_embeddings = torch.sparse.mm(adj, ego_embeddings)
+#             print(side_embeddings.device)
+            sum_embeddings = F.leaky_relu(self.GC_Linear_list[i](side_embeddings))
+            bi_embeddings = torch.mul(ego_embeddings, side_embeddings)
+            bi_embeddings = F.leaky_relu(self.Bi_Linear_list[i](bi_embeddings))
+            ego_embeddings = sum_embeddings + bi_embeddings
+            ego_embeddings = self.dropout_list[i](ego_embeddings)
+
+            norm_embeddings = F.normalize(ego_embeddings, p=2, dim=1)
+            all_embeddings += [norm_embeddings]
+
+        # all_embeddings = torch.cat(all_embeddings, dim=1)
+        all_embeddings = torch.stack(all_embeddings,dim = 1)
+        all_embeddings = torch.mean(all_embeddings, dim=1)
+        u_g_embeddings, i_g_embeddings = torch.split(all_embeddings, [self.user_num, self.item_num], dim=0)
+        return u_g_embeddings, i_g_embeddings
+
+    #NGCF user and category
+    def UC(self,adj):
+        ego_embeddings = torch.cat((self.user_emb.weight, self.category_emb.weight), dim=0).to(self.dev)
+        all_embeddings = [ego_embeddings]
+        for i in range(self.n_layers):
+            side_embeddings = torch.sparse.mm(adj, ego_embeddings)
+#             print(side_embeddings.device)
+            sum_embeddings = F.leaky_relu(self.GC_Linear_list[i](side_embeddings))
+            bi_embeddings = torch.mul(ego_embeddings, side_embeddings)
+            bi_embeddings = F.leaky_relu(self.Bi_Linear_list[i](bi_embeddings))
+            ego_embeddings = sum_embeddings + bi_embeddings
+            ego_embeddings = self.dropout_list[i](ego_embeddings)
+
+            norm_embeddings = F.normalize(ego_embeddings, p=2, dim=1)
+            all_embeddings += [norm_embeddings]
+
+        # all_embeddings = torch.cat(all_embeddings, dim=1)
+        all_embeddings = torch.stack(all_embeddings,dim = 1)
+        all_embeddings = torch.mean(all_embeddings, dim=1)
+        u_g_embeddings, i_g_embeddings = torch.split(all_embeddings, [self.user_num, self.cate_num], dim=0)
+        return u_g_embeddings, i_g_embeddings
+
---
+++ re_TIAC_test/utils.py
@@ -0,0 +1,256 @@
+import numpy as np
+from collections import defaultdict
+from datetime import datetime
+from datetime import timedelta
+from scipy import sparse as sp
+from dateutil.parser import parse
+import torch
+
+class DateSet(object):
+    def __init__(self, path):
+        self.train_path = "./data/"+ path + '.txt'
+        self.path = ''
+        user_train = self.data_partition()
+        self.time_csv, self.t_map_csv = self.mappingAndSort(user_train)
+        self.getItemtime()
+
+    def data_partition(self, ):
+        user_train = defaultdict(list)
+        with open(self.train_path, 'r') as f:
+            for line in f.readlines():
+                u, i, c, timestamp = line.rstrip().split(' ')
+                u = int(u)
+                i = int(i)
+                c = int(c)
+                try:
+                    timestamp = int(timestamp)
+                except:
+                    timestamp = float(timestamp)
+                user_train[u].append([i, c, timestamp])
+        return user_train
+
+    def mappingAndSort(self, user_train):
+
+        user_csv = set()
+        item_csv = set()
+        cate_csv = set()
+        day_csv = set()
+        year_csv = set()
+        month_csv = set()
+
+        for u, info in user_train.items():
+            # l = len(info)
+            user_csv.add(u)
+            for i in info:
+                item_csv.add(i[0])
+                cate_csv.add(i[1])
+                t = datetime.fromtimestamp(i[2])
+                
+                day_csv.add(int(t.strftime('%j')))
+                year_csv.add(t.year)
+                month_csv.add(t.month)
+        
+        self.user_num, self.item_num,self.cate_num  = len(user_csv) + 1, len(item_csv) + 1, len(cate_csv) + 1
+        self.day_num, self.year_num, self.month_num = len(day_csv)+1, len(year_csv)+1, len(month_csv)+1
+        
+        u_map_csv = dict(zip(user_csv, [i+1 for i in range(len(sorted(user_csv)))]))
+        i_map_csv = dict(zip(item_csv, [i+1 for i in range(len(sorted(item_csv)))]))
+        c_map_csv = dict(zip(cate_csv, [i+1 for i in range(len(sorted(cate_csv)))]))
+
+        d_map_csv = dict(zip(day_csv, [i+1 for i in range(len(sorted(day_csv)))]))
+        self.y_map_csv = dict(zip(year_csv, [i+1 for i in range(len(sorted(year_csv)))]))
+        m_map_csv = dict(zip(month_csv, [i+1 for i in range(len(sorted(month_csv)))]))
+
+        self.User_train = defaultdict(list)
+        self.User_train_cate = defaultdict(list)
+        for u, info in user_train.items():
+            sorted_info = sorted(info, key=lambda x: x[2])
+            sorted_info = list(map(lambda x: [i_map_csv[x[0]], [self.y_map_csv[datetime.fromtimestamp(x[2]).year], 
+                                               m_map_csv[datetime.fromtimestamp(x[2]).month],
+                                               d_map_csv[int(datetime.fromtimestamp(x[2]).strftime('%j'))]]], sorted_info))
+            sorted_info_cate = sorted(info, key=lambda x: x[2])
+            sorted_info_cate = list(map(lambda x: [c_map_csv[x[1]], [self.y_map_csv[datetime.fromtimestamp(x[2]).year], 
+                                               m_map_csv[datetime.fromtimestamp(x[2]).month],
+                                               d_map_csv[int(datetime.fromtimestamp(x[2]).strftime('%j'))]]], sorted_info_cate))
+
+            self.User_train[u_map_csv[u]] = sorted_info
+            self.User_train_cate[u_map_csv[u]] = sorted_info_cate
+
+
+        print("data processing done...")
+        return 1, 2
+
+    def split_train_and_test(self, ):
+        user_train = {}
+        user_valid = {}
+        user_test = {}
+        for user in self.User_train:
+            nfeedback = len(self.User_train[user])
+            if nfeedback < 3:
+                user_train[user] = self.User_train[user]
+                user_valid[user] = []
+                user_test[user] = []
+            else:
+                user_train[user] = self.User_train[user][:-2]
+                user_valid[user] = []
+                user_valid[user].append(self.User_train[user][-2])
+                user_test[user] = []
+                user_test[user].append(self.User_train[user][-1])
+
+        return [user_train, user_valid, user_test, self.user_num, self.item_num, self.year_num, self.month_num, self.day_num]
+
+    def split_cate_train_and_test(self, ):
+        user_train = {}
+        user_valid = {}
+        user_test = {}
+        for user in self.User_train_cate:
+            nfeedback = len(self.User_train_cate[user])
+            if nfeedback < 3:
+                user_train[user] = self.User_train_cate[user]
+                user_valid[user] = []
+                user_test[user] = []
+            else:
+                user_train[user] = self.User_train_cate[user][:-2]
+                user_valid[user] = []
+                user_valid[user].append(self.User_train_cate[user][-2])
+                user_test[user] = []
+                user_test[user].append(self.User_train_cate[user][-1])
+
+        return [user_train, user_valid, user_test,self.cate_num]
+
+    def getItemtime(self):
+        adj_mat = sp.dok_matrix((self.item_num, self.year_num + self.month_num + self.day_num), dtype=np.float32)
+        for u, info in self.User_train.items():
+            for i in info:
+                adj_mat[i[0], i[1][0]] += 1
+                adj_mat[i[0], i[1][1] + self.year_num] += 1
+                adj_mat[i[0], i[1][2] + self.year_num + self.month_num] += 1
+
+        adj_mat = adj_mat.tocsr()
+        rowsum = np.array(adj_mat.sum(axis=1))
+        d_inv = np.power(rowsum, -1).flatten()
+        d_inv[np.isinf(d_inv)] = 0.
+        d_mat = sp.diags(d_inv)
+        adj_mat = d_mat.dot(adj_mat)
+        self.adj_mat = self._convert_sp_mat_to_sp_tensor(adj_mat)
+
+    #时间与项目进行卷积
+    def UCGraph(self,data):
+        adj_mats = sp.dok_matrix((self.user_num, self.cate_num), dtype=np.float32)
+        for u, info in data.items():
+            for i in info:
+                adj_mats[u, i[0]] += 1
+
+        adj_mat = sp.dok_matrix((self.user_num + self.cate_num, self.user_num + self.cate_num), dtype=np.float32)
+        adj_mat = adj_mat.tolil()
+        R = adj_mats.tolil()
+        adj_mat[:self.user_num, self.user_num:] = R
+        adj_mat[self.user_num:, :self.user_num] = R.T
+        adj_mat = adj_mat.todok()
+
+        def normalized_adj_single(adj):
+            rowsum = np.array(adj.sum(1))
+
+            d_inv = np.power(rowsum, -0.5).flatten()
+            d_inv[np.isinf(d_inv)] = 0.
+            d_mat_inv = sp.diags(d_inv)
+
+            norm_adj = d_mat_inv.dot(adj)
+            # norm_adj = adj.dot(d_mat_inv)
+            print('generate single-normalized adjacency matrix.')
+            return norm_adj.tocoo()
+        norm_adj_mat = normalized_adj_single(adj_mat + sp.eye(adj_mat.shape[0]))
+        
+        norm_time_adj = norm_adj_mat.tocsr()
+        norm_time_adj = self._convert_sp_mat_to_sp_tensor(norm_time_adj).float()
+        return norm_time_adj
+    
+
+    def UIGraph(self,data):
+        adj_mats = sp.dok_matrix((self.user_num, self.item_num), dtype=np.float32)
+        for u, info in data.items():
+            for i in info:
+                adj_mats[u, i[0]] += 1
+        adj_mat = sp.dok_matrix((self.user_num + self.item_num, self.user_num + self.item_num), dtype=np.float32)
+        adj_mat = adj_mat.tolil()
+        R = adj_mats.tolil()
+        adj_mat[:self.user_num, self.user_num:] = R
+        adj_mat[self.user_num:, :self.user_num] = R.T
+        adj_mat = adj_mat.todok()
+        
+#         adj_mat = adj_mat + sp.eye(adj_mat.shape[0])
+
+#         rowsum = np.array(adj_mat.sum(axis=1))
+#         d_inv = np.power(rowsum, -1).flatten()
+#         d_inv[np.isinf(d_inv)] = 0.
+#         d_mat = sp.diags(d_inv)
+
+#         norm_time_adj = d_mat.dot(adj_mat)
+#         norm_time_adj = norm_time_adj.dot(d_mat)
+        def normalized_adj_single(adj):
+            rowsum = np.array(adj.sum(1))
+
+            d_inv = np.power(rowsum, -0.5).flatten()
+            d_inv[np.isinf(d_inv)] = 0.
+            d_mat_inv = sp.diags(d_inv)
+
+            norm_adj = d_mat_inv.dot(adj)
+            # norm_adj = adj.dot(d_mat_inv)
+            print('generate single-normalized adjacency matrix.')
+            return norm_adj.tocoo()
+        norm_adj_mat = normalized_adj_single(adj_mat + sp.eye(adj_mat.shape[0]))
+        
+        norm_time_adj = norm_adj_mat.tocsr()
+        norm_time_adj = self._convert_sp_mat_to_sp_tensor(norm_time_adj).float()
+        return norm_time_adj
+
+    def _convert_sp_mat_to_sp_tensor(self, X):
+        coo = X.tocoo().astype(np.float32)
+        row = torch.Tensor(coo.row).long()
+        col = torch.Tensor(coo.col).long()
+        index = torch.stack([row, col])
+        data = torch.FloatTensor(coo.data)
+        return torch.sparse.FloatTensor(index, data, torch.Size(coo.shape))
+
+    def date_conversation(self,year, day):
+        year = int(year)
+        day = int(day)
+        first_day = datetime(year, 1, 1)
+        wanted_day = first_day + timedelta(day - 1)
+        wanted_day = datetime.strftime(wanted_day, '%Y%m%d')
+        return wanted_day
+
+    def get_year_key(self,value):
+        return [k for k, v in self.y_map_csv.items() if v == value]
+
+    def time_int(self,user_train,user_valid):
+    # 购买时间序列
+        his_time = np.zeros([len(user_train) + 1, 20])
+        for i, row in enumerate(user_train):
+            for j, val in enumerate(user_train[row]):
+                if (20 - len(user_train[row]) + j) >= 0:
+                    his_time[row][20 - len(user_train[row]) + j] = self.date_conversation(
+                        self.get_year_key(user_train[row][j][1][0])[0], user_train[row][j][1][2])
+    # 推荐时间
+        user_rec = np.zeros([len(user_train) + 1, 1])
+        for i, row in enumerate(user_valid):
+            if len(user_valid[row]) > 0:
+                user_rec[row][0] = self.date_conversation(self.get_year_key(user_valid[row][0][1][0])[0], user_valid[row][0][1][2])
+    #时间差
+        time_int = np.zeros([len(user_train) + 1, 20])
+        for i, row in enumerate(user_rec):
+            if row > 0:
+                a = user_rec[i]
+                a = parse(str(int(a[0])))
+                for j, num in enumerate(his_time[i]):
+                    b = num
+                    if b > 0:
+                        b = parse(str(int(b)))
+                        day = (a - b).days
+                        if day > 1024:
+                            day = 1024
+                        if day < 0:
+                            day = 0
+                        time_int[i][j] = day
+        print("time interval processing done...")
+        return time_int
